e3698e00a7e318e7974db50dcdc1e5af48872103
Donmikko Dan M. Olmillo <57443799+mikkolmillo@users.noreply.github.com>
2022-05-25 17:55:41 +0800

    add print function

--- components/pos/POSTItemsList.jsx
+++ components/pos/POSTItemsList.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { XCircleIcon } from '@heroicons/react/outline'
+import { ComponentToPrint } from '../utils/ComponentToPrint'
+import { useReactToPrint } from 'react-to-print'
 
 const POSTItemsList = () => {
   const [products, setProducts] = useState([])
@@ -68,6 +70,15 @@ const POSTItemsList = () => {
     setCart(newCart)
   }
 
+  const componentRef = useRef()
+  const handleReactToPrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
+  const handlePrint = () => {
+    handleReactToPrint()
+  }
+
   return (
     <div className="relative pt-16 pb-32 overflow-hidden">
       <div aria-hidden="true" className="absolute inset-x-0 top-0 h-48 bg-gradient-to-b from-gray-100" />
@@ -93,7 +104,30 @@ const POSTItemsList = () => {
           </div>
           <div className="mt-12 sm:mt-16 lg:mt-0">
             <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-              {/* <div className="-mx-4 mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
+              <div className="hidden">
+                <ComponentToPrint
+                  cart={cart}
+                  totalAmount={totalAmount}
+                  ref={componentRef}
+                />
+              </div>
+              <div className="sm:flex sm:items-center">
+                <div className="sm:flex-auto">
+                  <h1 className="text-xl font-semibold text-gray-900">Invoice</h1>
+                </div>
+                <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                  {totalAmount > 0 && (
+                    <button
+                      type="button"
+                      onClick={handlePrint}
+                      className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+                    >
+                      Print
+                    </button>
+                  )}
+                </div>
+              </div>
+              <div className="-mx-4 mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
                 <table className="min-w-full divide-y divide-gray-300">
                   <thead className="bg-gray-50">
                     <tr>
@@ -136,105 +170,13 @@ const POSTItemsList = () => {
                           <td className="px-3 py-4 text-sm text-gray-500">{item.totalAmount}</td>
                           <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                             <button className="text-indigo-600 hover:text-indigo-900" onClick={() => removeToCart(item)}>
-                              Remove<span className="sr-only">, {item.title}</span>
+                              <XCircleIcon className="h-6 w-6" aria-hidden="true" /><span className="sr-only">, {item.title}</span>
                             </button>
                           </td>
                         </tr>
                       ))
                     )}
                   </tbody>
-                </table>
-              </div> */}
-              <div className="sm:flex sm:items-center">
-                <div className="sm:flex-auto">
-                  <h1 className="text-xl font-semibold text-gray-900">Invoice</h1>
-                </div>
-                <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                  <button
-                    type="button"
-                    className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                  >
-                    Print
-                  </button>
-                </div>
-              </div>
-              <div className="-mx-4 mt-8 flex flex-col sm:-mx-6 md:mx-0">
-                <table className="min-w-full divide-y divide-gray-300">
-                  <thead>
-                    <tr>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 md:pl-0"
-                      >
-                        ID
-                      </th>
-                      <th
-                        scope="col"
-                        className="hidden py-3.5 px-3 text-right text-sm font-semibold text-gray-900 sm:table-cell"
-                      >
-                        Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="hidden py-3.5 px-3 text-right text-sm font-semibold text-gray-900 sm:table-cell"
-                      >
-                        Price
-                      </th>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-3 pr-4 text-right text-sm font-semibold text-gray-900 sm:pr-6 md:pr-0"
-                      >
-                        Quantity
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {cart && cart.map((item) => (
-                      <tr key={item.id} className="border-b border-gray-200">
-                        <td className="py-4 pl-4 pr-3 text-sm sm:pl-6 md:pl-0">
-                          <div className="font-medium text-gray-900">{item.id}</div>
-                        </td>
-                        <td className="hidden py-4 px-3 text-right text-sm text-gray-500 sm:table-cell">{item.title}</td>
-                        <td className="hidden py-4 px-3 text-right text-sm text-gray-500 sm:table-cell">{item.price}</td>
-                        <td className="py-4 pl-3 pr-4 text-right text-sm text-gray-500 sm:pr-6 md:pr-0">{item.qty}</td>
-                        <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                          <button className="text-indigo-600 hover:text-indigo-900" onClick={() => removeToCart(item)}>
-                            <XCircleIcon className="h-6 w-6" aria-hidden="true" /><span className="sr-only">, {item.title}</span>
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                      <th
-                        scope="row"
-                        colSpan={3}
-                        className="hidden pl-6 pr-3 pt-6 text-right text-sm font-normal text-gray-500 sm:table-cell md:pl-0"
-                      >
-                        Subtotal
-                      </th>
-                      <th scope="row" className="pl-4 pr-3 pt-6 text-left text-sm font-normal text-gray-500 sm:hidden">
-                        Subtotal
-                      </th>
-                      <td className="pl-3 pr-4 pt-6 text-right text-sm text-gray-500 sm:pr-6 md:pr-0">${totalAmount}</td>
-                    </tr>
-                    <tr>
-                      <th
-                        scope="row"
-                        colSpan={3}
-                        className="hidden pl-6 pr-3 pt-4 text-right text-sm font-semibold text-gray-900 sm:table-cell md:pl-0"
-                      >
-                        Total
-                      </th>
-                      <th scope="row" className="pl-4 pr-3 pt-4 text-left text-sm font-semibold text-gray-900 sm:hidden">
-                        Total
-                      </th>
-                      <td className="pl-3 pr-4 pt-4 text-right text-sm font-semibold text-gray-900 sm:pr-6 md:pr-0">
-                        ${totalAmount}
-                      </td>
-                    </tr>
-                  </tfoot>
                 </table>
               </div>
             </div>
---
+++ components/utils/ComponentToPrint.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+export const ComponentToPrint = React.forwardRef((props, ref) => {
+  const { cart, totalAmount } = props
+  return (
+    <div ref={ref} className="px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-col">
+        <table className="min-w-full divide-y divide-gray-300">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 md:pl-0"
+              >
+                ID
+              </th>
+              <th
+                scope="col"
+                className="hidden py-3.5 px-3 text-right text-sm font-semibold text-gray-900 sm:table-cell"
+              >
+                Name
+              </th>
+              <th
+                scope="col"
+                className="hidden py-3.5 px-3 text-right text-sm font-semibold text-gray-900 sm:table-cell"
+              >
+                Price
+              </th>
+              <th
+                scope="col"
+                className="py-3.5 pl-3 pr-4 text-right text-sm font-semibold text-gray-900 sm:pr-6 md:pr-0"
+              >
+                Quantity
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart && cart.map((item) => (
+              <tr key={item.id} className="border-b border-gray-200">
+                <td className="py-4 pl-4 pr-3 text-sm sm:pl-6 md:pl-0">
+                  <div className="font-medium text-gray-900">{item.title}</div>
+                  <div className="mt-0.5 text-gray-500 sm:hidden">
+                    {item.qty}pcs at {item.price}
+                  </div>
+                </td>
+                <td className="hidden py-4 px-3 text-right text-sm text-gray-500 sm:table-cell">{item.title}</td>
+                <td className="hidden py-4 px-3 text-right text-sm text-gray-500 sm:table-cell">{item.price}</td>
+                <td className="py-4 pl-3 pr-4 text-right text-sm text-gray-500 sm:pr-6 md:pr-0">{item.qty}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <th
+                scope="row"
+                colSpan={3}
+                className="hidden pl-6 pr-3 pt-6 text-right text-sm font-normal text-gray-500 sm:table-cell md:pl-0"
+              >
+                Subtotal
+              </th>
+              <th scope="row" className="pl-4 pr-3 pt-6 text-left text-sm font-normal text-gray-500 sm:hidden">
+                Subtotal
+              </th>
+              <td className="pl-3 pr-4 pt-6 text-right text-sm text-gray-500 sm:pr-6 md:pr-0">${totalAmount}</td>
+            </tr>
+            <tr>
+              <th
+                scope="row"
+                colSpan={3}
+                className="hidden pl-6 pr-3 pt-4 text-right text-sm font-semibold text-gray-900 sm:table-cell md:pl-0"
+              >
+                Total
+              </th>
+              <th scope="row" className="pl-4 pr-3 pt-4 text-left text-sm font-semibold text-gray-900 sm:hidden">
+                Total
+              </th>
+              <td className="pl-3 pr-4 pt-4 text-right text-sm font-semibold text-gray-900 sm:pr-6 md:pr-0">
+                ${totalAmount}
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  );
+});
