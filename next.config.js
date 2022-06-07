5d594cdc1a3e754d46a468044abd0a062c8253a2
Donmikko Dan M. Olmillo <57443799+mikkolmillo@users.noreply.github.com>
2022-06-07 16:48:33 +0800

    modify next config with env variables

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = (phase) => {
       reactStrictMode: true,
       env: {
         BASE_URL: 'http://localhost:3000',
-        // DATABASE_URL: "postgresql://postgres:mikkolmillo@localhost:5432/pos-3d-planner?schema=public"
+        DATABASE_URL: "postgresql://postgres:mikkolmillo@localhost:5432/pos-3d-planner?schema=public"
       }
     }
   }
@@ -16,7 +16,7 @@ const nextConfig = (phase) => {
     reactStrictMode: true,
     env: {
       BASE_URL: 'https://next-pos-sable.vercel.app',
-      // DATABASE_URL: "postgresql://akjfpsejleqfjq:50fa1a6864319e6c0dd6d65cc6e9720bce3154a9ab72250fcb93e1a444fa2e3d@ec2-34-203-182-172.compute-1.amazonaws.com:5432/dbreb5muhc905b?schema=public"
+      DATABASE_URL: "postgresql://akjfpsejleqfjq:50fa1a6864319e6c0dd6d65cc6e9720bce3154a9ab72250fcb93e1a444fa2e3d@ec2-34-203-182-172.compute-1.amazonaws.com:5432/dbreb5muhc905b?schema=public"
     }
   }
 }
